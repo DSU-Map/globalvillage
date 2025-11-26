f0b60ec1eae3b06b2e0712276d4382d747d4641e
GAON213193 <ungaon654@gmail.com>
2025-11-26 13:46:37 +0900

    Update save_global_menu.py
    같다면 유지 다르면 변경 업데이트

--- save_global_menu.py
+++ save_global_menu.py
@@ -1,15 +1,27 @@
 import json
+import os
 from global_menu_reader import fetch_current_menu_from_web
 
 def save_global_menu():
-    # 1) 웹에서 최신 식단 데이터 가져오기
-    data = fetch_current_menu_from_web()
+    new_data = fetch_current_menu_from_web()
 
-    # 2) JSON 파일로 저장
+    # 기존 JSON 있으면 로딩
+    if os.path.exists("global_menu.json"):
+        with open("global_menu.json", "r", encoding="utf-8") as f:
+            old_data = json.load(f)
+
+        # 같으면 업데이트 X
+        if new_data == old_data:
+            print("변화 없음 → 업데이트 생략")
+            return False
+
+    # 다르면 새로 저장
     with open("global_menu.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+        json.dump(new_data, f, ensure_ascii=False, indent=2)
 
-    print("저장 완료: global_menu.json")
+    print("변화 감지 → JSON 업데이트 완료")
+    return True
+
 
 if __name__ == "__main__":
     save_global_menu()
